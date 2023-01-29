1ca9176a707eedbdf27d2be49f2e40056356cf44
PatrickCelis <geometrydashforce@gmail.com>
2023-01-29 11:58:51 -0500

    Primer commit agregando estructura frontend

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(): JSX.Element {
+  
+  return (
+    <div>
+
+
+    </div>
+  );
+}
+
+export default App;

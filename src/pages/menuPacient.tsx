e410ba4f682c62e365deecec33b7cc82439dffcc
Brayan Torres <torresacunabrayan460@gmail.com>
2023-02-04 12:12:56 -0500

    Conectar vista menu pacientes con vista mis citas

--- src/pages/menuPacient.tsx
+++ src/pages/menuPacient.tsx
@@ -1,15 +1,10 @@
 import CalendarReact from "../components/CalendarReact";
-import { useNavigate } from "react-router-dom";
+import { redirect, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const MenuPacient = () => {
   const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate("/paciente/agendar-cita");
-  };
-
   return (
     <>
       <Navbar />
@@ -18,15 +13,15 @@ const MenuPacient = () => {
           <div className="col-8">
             <div className="d-grid gap-2 col-8 mx-auto">
               <button
-                onClick={handleClick}
+                onClick={()=>navigate("/paciente/agendar-cita")}
                 className="btn btn-primary"
                 type="button"
               >
                 Agendar Cita
               </button>
-              <a className="btn btn-secondary" type="button">
+              <button className="btn btn-secondary" type="button" onClick={()=> navigate("/citas")}>
                 Mis Citas
-              </a>
+              </button>
             </div>
           </div>
           <div className="col-sm-12 col-md-6 col-xl-4 mb-12">

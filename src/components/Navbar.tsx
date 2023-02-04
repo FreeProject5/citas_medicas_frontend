70b13ec8f6e2cf58d4ec1d6f1ed0cdda4f23c5aa
Brayan Torres <torresacunabrayan460@gmail.com>
2023-02-04 00:06:11 -0500

    LocalStorage Token

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,56 +1,49 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import logo from "../public/images/logo.png"
+import logo from "../public/images/logo.png";
 
 const Navbar = () => {
-  const navigate = useNavigate() 
+  const isLogged = false;
+  const navigate = useNavigate();
   const ManejarClick = () => {
-    navigate ("/signup")
-
-  }
+    navigate("/signup");
+  };
   const ManejarClick2 = () => {
-    navigate ("/login")
-
-  }
+    navigate("/login");
+  };
   return (
     <div className="header_section">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="logo">
           <a href="/">
-            <img src = {logo} className = "logo" alt = "logo"/>
-            
+            <img src={logo} className="logo" alt="logo" />
           </a>
         </div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">
-                Inicio
-              </a>
-            </li>
-            <li className="nav-item">
-              <button className="nav-link" onClick={ManejarClick2}>
-                Inicia Sesión
-              </button>
-            </li>
-            <li className="nav-item">
-              <button className="nav-link" onClick={ManejarClick}>
-                Registrate
-              </button>
-            </li>
-          </ul>
-        </div>
+
+        {isLogged ? (
+          <Link to="/logout">Logout</Link>
+        ) : (
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">
+                  Inicio
+                </a>
+              </li>
+              <li className="nav-item">
+                <button className="nav-link" onClick={ManejarClick2}>
+                  Inicia Sesión
+                </button>
+              </li>
+              <li className="nav-item">
+                <button className="nav-link" onClick={ManejarClick}>
+                  Registrate
+                </button>
+              </li>
+            </ul>
+          </div>
+        )}
       </nav>
     </div>
   );
---
+++ src/store/auth.ts
@@ -0,0 +1,41 @@
+import create from "zustand";
+import { persist } from "zustand/middleware";
+
+type State = {
+  token: string;
+  user: any;
+  isAuth:boolean
+};
+
+type Actions = {
+  setToken: (token: string) => void;
+  setUser: (user:any) =>void
+  logout:()=> void
+};
+
+export const useAuthStore = create(
+  persist<State & Actions>(
+    (set) => ({
+      token: "",
+      user:null,
+      isAuth:false,
+      setToken: (token: string) =>
+        set((state) => ({
+          token,
+          isAuth:true
+        })),
+      setUser: (user:any) =>
+        set((state) => ({
+          user,
+        })),
+      logout:()=>({
+        token:"",
+        user:null,
+        isAuth:false
+      })
+    }),
+    {
+      name: "auth",
+    }
+  )
+);

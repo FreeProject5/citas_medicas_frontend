296117e0c3ce15fd1d829c9e78d39324b76568ac
Brayan Torres <torresacunabrayan460@gmail.com>
2023-02-07 14:03:36 -0500

    Agregando seccion de cita

--- src/pages/menuPacient.tsx
+++ src/pages/menuPacient.tsx
@@ -1,5 +1,6 @@
 import CalendarReact from "../components/CalendarReact";
 import { redirect, useNavigate } from "react-router-dom";
+import { useAuthStore } from "../store/auth";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
@@ -8,29 +9,45 @@ const MenuPacient = () => {
   return (
     <>
       <Navbar />
-      <div className="container">
+      <div className="container mb-4">
         <div className="row align-items-center">
           <div className="col-8">
-            <div className="d-grid gap-2 col-8 mx-auto">
+            <div className="d-grid gap-2 col-8 mx-auto mt-4 mb-5">
               <button
-                onClick={()=>navigate("/paciente/agendar-cita")}
+                onClick={() => navigate("/paciente/agendar-cita")}
                 className="btn btn-primary"
                 type="button"
               >
                 Agendar Cita
               </button>
-              <button className="btn btn-secondary" type="button" onClick={()=> navigate("/paciente/citas")}>
+              <button
+                className="btn btn-secondary"
+                type="button"
+                onClick={() => navigate("/paciente/citas")}
+              >
                 Mis Citas
               </button>
             </div>
           </div>
-          <div className="col-sm-12 col-md-6 col-xl-4 mb-12">
+          <div className="col-sm-12 col-md-6 col-xl-4 mb-5 mt-5">
             <CalendarReact />
           </div>
         </div>
-        <hr />
+        <br />
         <div className="row">
-          <div className="col-8"></div>
+          <div className="col-8">
+            <figure className="bg-white p-3 rounded quote-border">
+              <blockquote className="blockquote pb-2">
+                <p>
+                  El buen médico trata la enfermedad; el gran médico trata al
+                  paciente que tiene la enfermedad.
+                </p>
+              </blockquote>
+              <figcaption className="blockquote-footer mb-0 font-italic">
+              William Osler
+              </figcaption>
+            </figure>
+          </div>
           <div className="col-4">
             <div
               className="card position-relative top-0"

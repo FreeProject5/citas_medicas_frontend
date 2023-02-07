162b584140e8ff821956a387c3ce4c1b0505d33b
HansUrpay <105888827+HansUrpay@users.noreply.github.com>
2023-02-07 08:43:55 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Landing from './pages/landing'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useAuthStore } from './store/auth';
 import Login from './components/Login';
-import MenuPacient from './Pages/menuPacient';
-import AgendarCita from './Pages/agendarCita';
+import MenuPacient from './pages/menuPacient';
+import AgendarCita from './pages/agendarCita';
 import SignUp from './components/Signup';
 import Paginamedico from './medico/medicopag';
 import Paginacitas from './citas/citaspag';
